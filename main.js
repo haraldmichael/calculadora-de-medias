33d7537e95b8fbc25197f98eb8d834dda341b621
Harald Michael <harald.michael1@gmail.com>
2022-12-30 20:29:28 -0300

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ form.addEventListener(`submit`, function(e) {
 function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2)
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 
 
